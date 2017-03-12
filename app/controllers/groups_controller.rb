60203d098ad9df431c4f0a552432dea082d51f33
sean lin <seanlin0918@gmail.com>
2017-03-13 06:24:17 +1000

    implement groups#new

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,30 @@ class GroupsController < ApplicationController
     def index
         @groups = Group.all
     end
+
+    def new
+        @group = Group.new
+    end
+
+    def create
+        @group = Group.find(group_params)
+        @group.save
+        redirect_to groups_path, notice: 'Create Successfully'
+    end
+
+    def edit
+        @group = Group.find(params[:id])
+    end
+
+    def update
+        @group = Group.find(params[:id])
+        @group = Group.update(group_params)
+        redirect_to groups_path, notice: 'Update Successfully'
+    end
+
+    def destroy
+        @group = Group.find(params[:id])
+        @group.destroy
+        redirect_to groups_path, notice: 'Deleted'
+    end
 end
